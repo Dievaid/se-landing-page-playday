ee962a1024d0c2cb4266fbcda9dda53ea94fa705
dievaid <deividcapragiu@gmail.com>
2024-12-06 21:06:04 +0200

    Added provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Navbar } from './components/Navbar';
 import { Content } from './components/Content';
 import { Footer } from './components/Footer';
 
+import { PostHogProvider } from 'posthog-js/react';
+
+const apiKey = process.env.REACT_APP_POSTHOG_KEY;
+const options = {
+    api_host: process.env.REACT_APP_POSTHOG_HOST,
+}
+
 export const App : React.FC = () => {
+    useEffect(() => {
+        console.log(apiKey, options);
+    }, []);
+
     return (
-        <>
+        <PostHogProvider
+            apiKey={apiKey}
+            options={options}
+        >
             <Navbar />
             <Content />
             <Footer />
-        </>
+        </PostHogProvider>
     );
 }
